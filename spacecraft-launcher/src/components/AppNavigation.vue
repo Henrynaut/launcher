9db984ea7d0d8744380d0095daf04cb334f9e3b5
Neil McHenry <neilmchenry@gmail.com>
2019-09-22 14:06:47 -0500

    Added Bubble Hamburger sidebar Menu

--- spacecraft-launcher/src/components/AppNavigation.vue
+++ spacecraft-launcher/src/components/AppNavigation.vue
@@ -7,13 +7,13 @@
                         <v-list-tile-content>
                             {{item.title}}
                         </v-list-tile-content>
-                    </v-list-tile>
+                    </v-list-tile>  
                     <v-divider :key="`divider-${index}`"></v-divider>
                 </template>
             </v-list>
         </v-navigation-drawer>
         <v-toolbar app color="blue-grey darken-3" dark>
-            <v-toolbar-side-icon class="hidden-md-and-up" @click="drawer = !drawer"></v-toolbar-side-icon>
+            <!-- <v-toolbar class="hidden-md-and-up" @click="drawer = !drawer"></v-toolbar> -->
             <v-spacer class="hidden-md-and-up"></v-spacer>
             <v-toolbar-title>{{appTitle}}</v-toolbar-title>
             <v-btn flat class="hidden-sm-and-down">Menu</v-btn>
@@ -21,10 +21,16 @@
             <v-btn flat class="hidden-sm-and-down">SIGN IN</v-btn>
             <v-btn color="blue-grey lighten-3" class="hidden-sm-and-down">JOIN</v-btn>
         </v-toolbar>
+        <Bubble>
+            <a id="home" href="#">
+                <span>Home</span>
+            </a>
+        </Bubble>
     </span>
 </template>
 
 <script>
+import { Bubble } from 'vue-burger-menu'  // import the CSS transitions you wish to use, in this case we are using `Bubble`
 export default {
     name: 'AppNavigation',
     data() {
@@ -37,6 +43,9 @@ export default {
                 { title: 'Join' }
             ]
         };
+    },
+    components: {
+    Bubble // Register your component
     }
 };
 </script>
